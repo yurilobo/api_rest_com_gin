470babbe1d131837feb0b33dc38b0faf6929ae95
Yuri <yuri.anderson.lobo@gamail.com>
2024-02-12 21:54:12 -0300

    primeira rota

--- main.go
+++ main.go
@@ -4,8 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func ExibeTodosOsAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"nome": "2",
+	})
+}
+
 func main() {
 	//
 	r := gin.Default()
+	r.GET("/alunos", ExibeTodosOsAlunos)
 	r.Run()
 }
