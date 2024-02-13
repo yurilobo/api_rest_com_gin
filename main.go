2d5d989da09c774fbf29dc97a8f1cf2aeb26afab
Yuri <yuri.anderson.lobo@gamail.com>
2024-02-13 09:55:16 -0300

    usando gorm para criar tabelas

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"api_com_gin/database"
 	"api_com_gin/models"
 	"api_com_gin/routes"
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "Gui Lima", CPF: "00000000000", RG: "47000000000"},
 		{Nome: "ANA Lima", CPF: "00000000001", RG: "47000000001"},
