cb08ff61304c57eda254ed67b34011f5977326e7
Yuri <yuri.anderson.lobo@gamail.com>
2024-02-14 09:55:01 -0300

    teste de requisicao com go

--- main_test.go
+++ main_test.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"api_com_gin/controllers"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func SetupDasRotasDeTeste() *gin.Engine {
 	rotas := gin.Default()
 	return rotas
 }
-func TestFalhador(t *testing.T) {
-	t.Fatalf("Testes falhou de proposito, não se preucupe")
+func TestVerificaSatusCodeDaSaudacaoComParametro(t *testing.T) {
+	r := SetupDasRotasDeTeste()
+	r.GET("/:nome", controllers.Saldacao)
+	req, _ := http.NewRequest("GET", "/Yuri", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Deveriam ser iguais")
+	mockDaResposta := `{"API diz: ":" E ai Yuri, tudo beleza?"}`
+	respostaBody, _ := ioutil.ReadAll(resposta.Body)
+	assert.Equal(t, mockDaResposta, string(respostaBody))
+	fmt.Println(string(respostaBody))
+	fmt.Println(mockDaResposta)
 }
